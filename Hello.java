7dc917d098ace1f299f1b48e2aba46aa4575d067
affanathaa <affan_pradita_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:01:48 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello{}
+    public static void main(String[] args) {
+        
+        String nama="atha";
+        String asalKota="malang";
+        int usia=15;
+        double beratBadan=50.00;
+        float tinggiBadan=170.0f;
+        char jenisKelamin= 'L';
+        boolean status = true;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Asal Kota: "+asalKota);
+        System.out.println("Usia: "+usia);
+        System.out.println("Berat Badan: "+beratBadan);
+        System.out.println("Tinggi Badan: "+tinggiBadan);
+        System.out.println("Jenis Kelamin: "+jenisKelamin);
+        System.out.println("Status: "+status);}
+
+
